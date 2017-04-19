882154c50590698227eefd2461f83b0afba2ccad
Eyo <andeladeveloper@Eyo-MacBook-Pro-2.local>
2017-04-19 06:15:16 +0100

    add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const BUILD_PATH = path.resolve(__dirname, 'public');
+const APP_DIR = path.resolve(__dirname);
+
+module.exports = {
+  devtool: 'cheap-eval-source-map',
+  entry: `${APP_DIR}/main.js`,
+  output: {
+    path: BUILD_PATH,
+    filename: 'index.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+
+    ],
+  },
+};
+
