807c88718b7ede2e7868bc9588f38eb96e3349a2
Eyo <andeladeveloper@Eyo-MacBook-Pro-2.local>
2017-04-19 06:09:36 +0100

    add entry point file

---
+++ main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import App from './src/App.js';
+
+const root = document.getElementById('app');
+ReactDom.render(<App/>, root);
