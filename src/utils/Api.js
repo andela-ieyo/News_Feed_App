c41dbaa40791ad0430cfca609ff061aa0e3b442a
Eyo <andeladeveloper@Eyo-MacBook-Pro-2.local>
2017-04-20 20:12:42 +0100

    refactor code for Api call

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 class Api {
   constructor() {
-    this.apiKey = '213327409d384371851777e7c7f78dfe',
+    this.apiKey = '213327409d384371851777e7c7f78dfe';
     this.linkString = 'https://newsapi.org/v1/articles?';
     this.apilink = 'https://newsapi.org/v1/sources?language=en';
     this.link = this.linkString;
